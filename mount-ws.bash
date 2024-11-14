2e5cdc980f611f6481c0beded71a34b8ce342e87
ProyectosAARCO <proyectosaarco@gmail.com>
2024-11-14 12:44:44 -0600

    Update mount-ws.bash

--- mount-ws.bash
+++ mount-ws.bash
@@ -1,5 +1,20 @@
+#!/bin/bash
+
 sudo mkdir -p /mnt/w1
 sudo mount -t drvfs A: /mnt/w1
 
 sudo mkdir -p /mnt/w2
 sudo mount -t drvfs B: /mnt/w2
+
+for dirve in {A...Z}; do
+  if [[ -e /mnt/$drive ]]; then
+    echo "La unidad $drive ya está montada"
+  elif [[ -e /mnt/c/Users/cesar.hernandezb ]]; then
+    if mount | grep -q "/mnt/$drive"; then
+      echo "La unidad $drive ya está montada."
+    else
+      echo "Montando la unidad $drive..."
+      sudo mount -t drvfs "$drive:" "/mnt/$drive"
+    fi
+  fi
+done
